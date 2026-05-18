6c5c76153e5af7a802e18bde15873bc1c6d404bc
Miguel <104658338+miguelpalacios1987@users.noreply.github.com>
2026-05-18 14:58:24 -0300

    Condicionais

--- obj/condicionais10.cs
+++ obj/condicionais10.cs
@@ -6,8 +6,16 @@ public class Codicionais5
     
     public static void Main(string[] args)
     {
-        
+        Console.WriteLine("Digite o Numero: ");
+        int numero = int.Parse(Console.ReadLine());
 
+        for(int i = 1;i <= 10; i++)
+        {
+            int num = numero;
+            int resultado = num * i;
+            Console.Write($"\n {num} * {i} = {resultado}");
+        }
+          Console.Write($"\nFim da tabulada do {numero}");
         
     }
 }
